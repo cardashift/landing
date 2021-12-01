ef8f27c189563df804f193129fb127c53669be60
mooadh <mouadh.f@littlebigsmala.com>
2021-12-01 22:20:22 +0100

    fix hero soacing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-20">
       <Header />
       <Hero />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
         none: 'none',
         DEFAULT: '-10px -10px 20px #FAFBFF, 10px 10px 20px #A6ABBD',
         md: '-10px -10px 20px rgba(250, 251, 255, 0.5), 10px 10px 20px rgba(166, 171, 189, 0.5)',
+        // btn: 'inset 5px 5px 10px #f7f7f7,inset -5px -5px 5px #FF7A00;;',
       },
       spacing: {
         40: '40px',
@@ -40,13 +41,12 @@ module.exports = {
         50: '50rem',
       },
       colors: {
+        paraColor: '#4D515C',
         // Configure your color palette here
         grayPublicICO: '#4D515C',
         orange: '#FF7A00',
+        orangeBlack: '#eb7100',
         BtnBorderColor: 'rgba(255, 255, 255, 0.4)',
-        // background: {
-        //   light: '#EBECF0',
-        // },
         blue: '#0029FF',
         blueRetro: '#0D0F3F',
         gray: '#4D515C',
