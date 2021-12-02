0f406f38d88c6df8e31c05187d473b0e7680ae53
mooadh <mouadh.f@littlebigsmala.com>
2021-12-02 11:49:51 +0100

    add bubble image

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -6,28 +6,35 @@ import TitleContainer from '../components/TitleContainer'
 function Hero() {
   return (
     <div style={{ width: 519, marginTop: 91 }}>
-      <TitleContainer
-        title={'Debug the world'}
-        paragraphe={
-          'Cardashift, the launchpad in service of social and environmental impact'
-        }
-        classnametitle=" text-8xl "
-        classname="mt-6"
-      />
-      <PublicICO />
-      <div className="flex justify-center">
-        <Button className="shadow bg-gradient-to-br from-orangeBlack via-orange to-orange font-medium text-base text-white h-14 tracking-wide">
-          Pre-register to the ico
-          <ArrowIcon className="ml-2" color="orange" />
-        </Button>
-        <Button className="shadow font-medium text-base ml-6 h-14 tracking-wide text-gray ">
-          Submit your project
-          <ArrowIcon color="gray" className="mx-2 " />
-        </Button>
+      <div>
+        {' '}
+        <TitleContainer
+          title={'Debug the world'}
+          paragraphe={
+            'Cardashift, the launchpad in service of social and environmental impact'
+          }
+          classnametitle=" text-8xl "
+          classname="mt-6"
+        />
+        <PublicICO />
+        <div className="flex justify-center">
+          <Button className="shadow bg-gradient-to-br from-orangeBlack via-orange to-orange font-medium text-base text-white h-14 tracking-wide">
+            Pre-register to the ico
+            <ArrowIcon className="ml-2" color="orange" />
+          </Button>
+          <Button className="shadow font-medium text-base ml-6 h-14 tracking-wide text-gray ">
+            Submit your project
+            <ArrowIcon color="gray" className="mx-2 " />
+          </Button>
+        </div>
+        <span className="flex items-center text-gray-600 text-xs my-24 font-Inter font-bold text-gray ">
+          POWERED BY <img className="ml-2" src="../images/powered-by.svg  " />
+        </span>
       </div>
-      <span className="flex items-center text-gray-600 text-xs my-24 font-Inter font-bold text-gray ">
-        POWERED BY <img className="ml-2" src="../images/powered-by.svg" />
-      </span>
+      <img
+        src="../images/bubble-purple.png"
+        class="absolute top-0 right-0 -z-1"
+      />
     </div>
   )
 }
