07a25e13b86973621237db0f2bfbee40b00c1148
houssem3 <houssem@littlebigsmala.com>
2021-12-03 16:44:34 +0100

    add square component

--- src/components/LogoCircle.js
+++ src/components/LogoCircle.js
@@ -8,34 +8,53 @@ function LogoCircle() {
       boxShadow:
         'inset -6.98093px -6.98093px 13.9619px #FAFBFF, inset 6.98093px 6.98093px 13.9619px #A6ABBD',
       borderRadius: '50%',
-      position: 'absolute',
+      position: 'relative',
     },
     largeCircle = {
       ...circle,
       right: '-680px',
       height: 1318,
       width: 1318,
+      square: {
+        transform: 'rotate(-28deg)',
+        bottom: 254,
+        left: 10,
+      },
     }
   const mediumCercle = {
     ...circle,
     width: 830,
     height: 830,
+    square: {
+      transform: 'rotate(33deg)',
+      top: 0,
+      left: 100,
+    },
   }
   const smallCircle = {
     ...circle,
     width: 410,
     height: 410,
+    square: {
+      top: 115,
+      left: -60,
+    },
   }
 
   return (
-    <div className="flex items-center justify-center" style={largeCircle}>
-      <RoundSquare icon="/images/arrows.svg" />
-      <span className="flex items-center justify-center" style={mediumCercle}>
-        <RoundSquare icon="/images/stim.svg" />
-        <span style={smallCircle}>
-          <RoundSquare icon="/images/matters.svg" />
+    <div className="absolute">
+      <div className="flex items-center justify-center" style={largeCircle}>
+        <RoundSquare style={largeCircle.square} icon="/images/stim.svg" />
+        <span className="flex items-center justify-center" style={mediumCercle}>
+          <RoundSquare style={mediumCercle.square} icon="/images/matters.svg" />
+          <span style={smallCircle}>
+            <RoundSquare
+              style={smallCircle.square}
+              icon="/images/arrows-icon.svg"
+            />
+          </span>
         </span>
-      </span>
+      </div>
     </div>
   )
 }
