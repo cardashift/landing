d33ea44d0d072499ce70d7acfa29a0d7bb8d6e66
mooadh <mouadh.f@littlebigsmala.com>
2021-12-03 11:04:49 +0100

    fix background of the roadmapCards section

---
+++ src/components/FundingCap.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function FundingCap({ topTittle, pricing }) {
+  return (
+    <div className="w-48 flex flex-col border-2 rounded-lg border-white shadow-sm bg-button-orange-gradien py-4 px-6 border-opacity-40 ">
+      <span className="text-blue uppercase font-Inter font-bold text-xs tracking-wider mb-1">
+        {topTittle}
+      </span>
+      <span className="font-Inter font-bold text-base text-primaire">
+        {pricing}
+      </span>
+    </div>
+  )
+}
+
+export default FundingCap
