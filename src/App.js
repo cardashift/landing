efda98db7f9172cc14faea330ba3fb5e357937a4
oussema dhouib <oussema.d@littlebigsmala.com>
2021-12-03 15:30:33 +0100

    add JoinCommunity Section

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import FundingStages from './sections/FundingStages'
 import Roadmap from './sections/Roadmap'
 import InitialToken from './sections/InititalToken'
 import Partners from './sections/Partners'
+import JoinCommunity from './sections/JoinCommunity'
+
 // import FeaturedMedia from './sections/FeaturedMedia'
 // import Profile from './components/Profile'
 // import PartnerCard from './components/PartnerCard'
@@ -26,6 +28,7 @@ function App() {
       <FundingStages />
       <Roadmap />
       <Partners />
+      <JoinCommunity />
       {/*<FeaturedMedia />
       <Profile
         urlPicture="/images/profile-2.png"
