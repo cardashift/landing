99d760a95ddd75936b94067dcdaa7c59b2088831
houssem3 <houssem@littlebigsmala.com>
2021-12-05 19:54:25 +0100

    feat: add roadmap section

--- src/components/RoadMapCard.js
+++ src/components/RoadMapCard.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import classnames from 'classnames'
-function RoadMapCard({ classname }) {
+function RoadMapCard({ classname, title, subTitle, children, titleColor }) {
   return (
-    <div className="relative p-5 w-1/6 m-6 rounded-lg h-52 border-white border-opacity-40 border-2 shadow-sm bg-button-orange-gradien bg-grayLight bg-blend-soft-light">
-      <h3 className="font-Inter font-bold text-lg ">Q2</h3>
-      <ul className="mt-4 px-2">
-        <li className="font-Inter text-sm font-normal mb-1">
-          &bull; Lorem Ipsum
-        </li>
-        <li className="font-Inter text-sm font-normal mb-1">
-          &bull; Lorem Ipsum
-        </li>
-        <li className="font-Inter text-sm font-normal mb-1">
-          &bull; Lorem Ipsum
-        </li>
-      </ul>
+    <div
+      style={{ height: 440 }}
+      className="relative p-5 w-1/3 m-6 rounded-lg h-full border-white border-opacity-40 border-2 shadow-sm bg-button-orange-gradien bg-grayLight bg-blend-soft-light"
+    >
       <div
+        style={{ width: 'calc(100% - 20px' }}
         className={classnames(
-          `${classname} h-1 w-11/12  border-2 absolute left-2 bottom-2 rounded-md`
+          `${classname} h-1  border-2 absolute left-2 top-2 rounded-md`
         )}
       ></div>
+      <h4
+        style={{ color: titleColor }}
+        className="font-Inter font-bold   text-xs"
+      >
+        {title}
+      </h4>
+      <h3 className="font-Inter font-bold text-lg ">{subTitle}</h3>
+      {children}
     </div>
   )
 }
--- src/sections/Roadmap.js
+++ src/sections/Roadmap.js
@@ -6,19 +6,74 @@ import { Element } from 'react-scroll'
 function Roadmap() {
   return (
     <Element name={'Roadmap'}>
-      <div className="hidden md:block">
+      <img
+        className="absolute right-0"
+        src="/images/right-semiCloud.svg"
+        style={{ paddingTop: 520 }}
+      />
+      <div className="hidden md:block h-full">
         <TitleContainer
           topTitle="Milestones"
           title="Cardashift roadmap"
           classnametitle="text-5xl font-black mt-3"
         />
         <div className="flex">
-          <RoadMapCard classname="border-blue" />
-          <RoadMapCard classname="border-blueLight" />
-          <RoadMapCard classname="border-blueCiel" />
-          <RoadMapCard classname="border-orangeLight" />
-          <RoadMapCard classname="border-orangeLight border-opacity-60" />
-          <RoadMapCard classname="border-orangeLight border-opacity-40" />
+          {/* <RoadMapCard classname="border-blue" />
+          <RoadMapCard classname="border-blueLight" /> */}
+          <RoadMapCard
+            classname="border-blueCiel"
+            title="First Era"
+            subTitle="Develop tech enablers"
+            titleColor="'#0029FF'"
+          >
+            <p className="font-Inter font-normal text-sm">
+              The objective of this era is double :<br />
+              <br />
+              Publish a Launchpad that is accessible to anyone, crypto users and
+              non crypto users (B2C).
+              <br />
+              <br />
+              1.This will be achieved in two steps : Launchpad v1.0 for crypto
+              enthusiasts, and v2.0 for B2C.
+              <br />
+              <br />
+              2.Release key enablers for our community, projects and Cardashift
+              such as DEX, Staking, Wallet, Farming…
+              <br />
+              <br />
+              By the end of 2022, we will be provided with an ecosystem of
+              products and services to enable projects to be funded and generate
+              impact at scale. We will also strengthen our assistance to the
+              first projects of the Launchpad.
+            </p>
+          </RoadMapCard>
+
+          <RoadMapCard
+            classname="border-orangeLight"
+            title="Second ErA"
+            subTitle="Go Mainstream and new products & services"
+            titleColor="'#FF7A00'"
+          >
+            <p className="font-Inter font-normal text-sm">
+              The second era aims to design and develop new products and
+              services for the B2C, while assisting the launchpad projects to
+              start and scale.
+            </p>
+          </RoadMapCard>
+          <RoadMapCard
+            classname="border-black"
+            title="Third Era"
+            subTitle="System shifts"
+            titleColor="#121E34"
+          >
+            <p className="font-Inter font-normal text-sm">
+              The goal of this era is to tackle systemic blockers to generate
+              new ecosystems that are completely different from the current one
+              we live in. Strategic insights will be delivered by our teams to
+              guide joined efforts.
+            </p>
+          </RoadMapCard>
+          {/* <RoadMapCard classname="border-orangeLight border-opacity-40" /> */}
         </div>
       </div>
     </Element>
